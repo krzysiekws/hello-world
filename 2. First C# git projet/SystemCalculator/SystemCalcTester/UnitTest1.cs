167bab999c548101a7d856e7f99fcbea123f6f54
krzysiekws <krzysiekws@gmail.com>
2018-11-14 23:45:43 +0100

    two test passed

---
+++ 2. First C# git projet/SystemCalculator/SystemCalcTester/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SystemCalculator;
+
+namespace SystemCalcTester
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Calc74in10to2()
+        {
+            var calc = new SystemCalc(10, 2, 74);
+            var result = calc.Calculate();
+            Assert.AreEqual("1001010", result);
+        }
+
+        [TestMethod]
+        public void Calc74in10to7()
+        {
+            var calc = new SystemCalc(10, 7, 74);
+            var result = calc.Calculate();
+            Assert.AreEqual("134", result);
+        }
+
+        [TestMethod]
+        public void Calc74in10to16()
+        {
+            var calc = new SystemCalc(10, 16, 74);
+            var result = calc.Calculate();
+            Assert.AreEqual("4A", result);
+        }
+    }
+}
--- 2. First C# git projet/SystemCalculator/SystemCalculator/Program.cs
+++ 2. First C# git projet/SystemCalculator/SystemCalculator/Program.cs
@@ -6,11 +6,61 @@ using System.Threading.Tasks;
 
 namespace SystemCalculator
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Podaj system oryginalny (2, 4, 8, 16, 10 itd)");
+            var oldSystem = Console.ReadLine();
+            Console.WriteLine("Podaj system dcelowy");
+            var newSystem = Console.ReadLine();
+            Console.WriteLine("Podaj wartość");
+            var valueToCalculate = Console.ReadLine();
+        }
 
+    }
+
+    public class SystemCalc
+    {
+        public int OldSys { get; set; }
+        public int NewSystem { get; set; }
+        public int ValueToCalc { get; set; }
+
+        public SystemCalc(int oldSystem, int newSystem, int valueToCalc)
+        {
+            this.OldSys = oldSystem;
+            this.NewSystem = newSystem;
+            this.ValueToCalc = valueToCalc;
+        }
+
+        public string Calculate()
+        {
+            switch (NewSystem)
+            {
+                case 2:
+                    return CalcStandardMethod();
+                case 7:
+                    return CalcStandardMethod();
+                case 16:
+                        throw new NotImplementedException();
+                default:
+                        throw new Exception("Not implemeted system");
+            }
+        }
+
+        public string CalcStandardMethod()
+        {            
+            string returnValue = null;
+            int loopValue = ValueToCalc;
+
+            while (loopValue > 0)
+            {
+                returnValue = loopValue % NewSystem + returnValue;
+                loopValue = loopValue / NewSystem;
+            }
+
+            return returnValue;
         }
     }
+
 }
